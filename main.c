69bd383ff29ce59068476cb0166e2fe0038b787b
TheCookieLeg <lucas2.olafsen@gmail.com>
2025-11-05 10:33:25 +0100

    Sorting algorithm works now

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
-void Merge(int L[], int start, int end, int mid);
+void Merge(int L[], int start, int mid, int end);
 void MergeSort(int L[], int start, int end);
 void printList(int L[], int size);
 
@@ -19,17 +20,53 @@ int main(void){
     return 0;
 }
 
-void Merge(int L[], int start, int end, int mid){
-    /*Here goes your code*/
+void Merge(int L[], int start, int mid, int end){
+    int n1 = mid - start + 1;    // number of elements in left
+    int n2 = end - mid;         // number of elements in right
+
+    // temporary arrays (VLAs; requires C99 or later)
+    int L1[n1];
+    int L2[n2];
+
+    // copy into temporaries
+    for (int i = 0; i < n1; i++) {
+        L1[i] = L[start + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        L2[j] = L[mid + 1 + j];
+    }
+
+    // merge back into L[start..end]
+    int i = 0, j = 0, k = start;
+    while (i < n1 && j < n2) {
+        if (L1[i] <= L2[j]) {
+            L[k++] = L1[i++];
+        } else {
+            L[k++] = L2[j++];
+        }
+    }
+
+    // copy any remaining elements of L1
+    while (i < n1) {
+        L[k++] = L1[i++];
+    }
+    // copy any remaining elements of L2
+    while (j < n2) {
+        L[k++] = L2[j++];
+    }
 }
 
 void MergeSort(int L[], int start, int end){
-    /*Here goes your code as well*/
+    if (start < end) {
+        int mid = floor((start + end) / 2);
+        MergeSort(L, start, mid);
+        MergeSort(L, mid + 1, end);
+        Merge(L, start, mid, end);
+    }
 }
 
 void printList(int L[], int size){
-    int i;
-    for (i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         printf("%d ", L[i]);
     printf("\n");
 }
